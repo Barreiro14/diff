f985f40571a0e2a232408f4fcd0979ca9b6f2e52
Gabriel Barreiro <elbarri@omar-pc>
2021-09-05 14:28:49 -0400

    derivate function fix

--- FDMsolonda.cpp
+++ FDMsolonda.cpp
@@ -17,26 +17,39 @@ void init(long double (*f)(long double), long double xi, long double xf){
    while (i < 1000){
         f0[i] = (*f)(xi);
         i = i + 1;
-        xi = xi + ((xf-xi)/1000)*i;
+        xi = xi + ((xf-xi)/999)*i;
    }    
 }
 
-void der(){
+void der(int i){
     /*
         Esta funcion asigna el valor aproximado de la derivada en 
     */
+   if (i == 0){
     F0[0] = 2.0*f0[1] - 1.5*f0[0] - 0.5*f0[2];
-    
-    F0[sizeof(f0) - 1] = 0.5*f0[sizeof(f0) - 3] - 2.0*f0[sizeof(f0) - 2] + 1.5*f0[sizeof(f0) - 1];
-    int i = 0;
-    while(i < sizeof(f0) - 1){
-        F0[i] = (f0[i + 1] - f0[i - 1])/2;
-        i = i + 1;
+   } else if (i == 999){ 
+    F0[999] = 0.5*f0[997] - 2.0*f0[998] + 1.5*f0[999];
+   } else{
+        int i = 1;
+        while(i < 999){
+            F0[i] = (f0[i + 1] - f0[i - 1])/2;
+            i = i + 1;
+        }
     }
 }
 
 
 int main(){
+    init(std::cos, 0.0, 2*M_PI_4);
+    int i = 0;
+    while (i < 1000){
+        der(i);
+        i = i + 1;
+    }
+    std::cout << f0[0] << std::endl;
+    std::cout << f0[999] << std::endl;
+    std::cout << F0[0] << std::endl;
+    std::cout << F0[999] << std::endl;
     return 0;
 }
 

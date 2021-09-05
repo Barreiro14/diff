91ed7650ae476a3991b158e5def176ff3ebfe636
Gabriel Barreiro <elbarri@omar-pc>
2021-09-05 19:46:38 -0400

    add grappher and bash script

---
+++ sol.sh
@@ -0,0 +1,5 @@
+./namefile #name of the c++ program
+
+python3 solgraph.py #name of the python program
+
+rm filename1.txt filename2.txt #name of the mean time files from C++ -> Python
---
+++ solgraph.py
@@ -0,0 +1,4 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open("filename1.txt", "r") #data file from C++

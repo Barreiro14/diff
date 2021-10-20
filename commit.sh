e6a50ca97222e5786e9cdb45d30a4e789b6826de
Gabriel Barreiro <elbarri@omar-pc>
2021-10-20 17:35:01 -0400

    commitname

--- commit.sh
+++ commit.sh
@@ -5,6 +5,6 @@ echo "Klk con este commit?"
 read commitname
 git commit -m commitname
 git push -u origin main
-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
-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
+echo "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"
+echo "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"
 echo "listo, ve y chequea"
